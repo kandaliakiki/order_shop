d9f104f95e50bcac86acc98a036b3d5f35854588
kandaliakiki <kandaliak1k1@gmail.com>
2024-09-23 11:13:14 +0700

    show category name on product list instead of all products

--- client/components/product_component/ProductListHeader.tsx
+++ client/components/product_component/ProductListHeader.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductListControls from "./ProductListControls";
 import { useProducts } from "./ProductContext";
+import { useCategories } from "./CategoryContext";
 
 const ProductListHeader = () => {
   const { products } = useProducts();
+  const { fetchCategoryById } = useCategories();
+  const [categoryName, setCategoryName] = useState<string>("All Products");
+
+  useEffect(() => {
+    window.addEventListener("updateSelectedCategory", () => {
+      // ...
+      const selectedCategory = localStorage.getItem("selectedCategory");
+      if (selectedCategory) {
+        fetchCategoryById(selectedCategory).then((category) => {
+          if (category) {
+            setCategoryName(category.name); // Assuming category has a 'name' property
+          }
+        });
+      } else {
+        setCategoryName("All Products");
+      }
+    });
+  }, []);
+
   return (
     <div className="flex justify-between items-center py-7">
-      <h1 className="text-2xl">All Products ({products.length})</h1>
+      <h1 className="text-2xl">
+        {categoryName} ({products.length})
+      </h1>
       <div className="flex gap-2 justify-between items-center">
         <ProductListControls />
       </div>
